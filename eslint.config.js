e9ebc0a668254c1ba070a44e1198196d0f3d0c2d
Pierre Bernard <pierrebernard14@gmail.com>
2025-07-28 18:29:04 +0200

    eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,20 @@ import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
+
+// Note the `/flat` suffix here, the difference from default entry is that
+// `/flat` added `name` property to the exported object to improve
+// [config-inspector](https://eslint.org/blog/2024/04/eslint-config-inspector/) experience.
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+
+const config = defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
 ]);
+
+export default [
+  ...config,
+  eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
+];
